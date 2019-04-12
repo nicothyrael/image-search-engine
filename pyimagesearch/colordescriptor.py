d27705ddb5efdcc0cc3ca75244fe050912eb332d
Nicolas Alonso <nalonsogb@sofi.org>
2019-04-12 18:16:04 -0300

    3ro

--- pyimagesearch/colordescriptor.py
+++ pyimagesearch/colordescriptor.py
@@ -50,21 +50,20 @@ class ColorDescriptor:
         # return the feature vector
         return features
 
+    def histogram(self, image, mask):
+        # extract a 3D color histogram from the masked region of the
+        # image, using the supplied number of bins per channel
+        hist = cv2.calcHist([image], [0, 1, 2], mask, self.bins,
+                            [0, 180, 0, 256, 0, 256])
 
-def histogram(self, image, mask):
-    # extract a 3D color histogram from the masked region of the
-    # image, using the supplied number of bins per channel
-    hist = cv2.calcHist([image], [0, 1, 2], mask, self.bins,
-                        [0, 180, 0, 256, 0, 256])
+        # normalize the histogram if we are using OpenCV 2.4
+        if imutils.is_cv2():
+            hist = cv2.normalize(hist).flatten()
 
-    # normalize the histogram if we are using OpenCV 2.4
-    if imutils.is_cv2():
-        hist = cv2.normalize(hist).flatten()
+        # otherwise handle for OpenCV 3+
+        else:
+            cv2.normalize(hist, hist)
+            hist = hist.flatten()
 
-    # otherwise handle for OpenCV 3+
-    else:
-        cv2.normalize(hist, hist)
-        hist = hist.flatten()
-
-    # return the histogram
-    return hist
+        # return the histogram
+        return hist
